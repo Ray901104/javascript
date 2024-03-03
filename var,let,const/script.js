613c2d39facc6fb9b18ea325864a9d4f25782d4d
yrcho <ip323@naver.com>
2024-03-03 17:33:39 +0900

    var, let, const

---
+++ var,let,const/script.js
@@ -0,0 +1,31 @@
+// 중복 선언 및 재할당 가능, 가급적 사용하지 않는다.
+var greeting = 'hello';
+console.log(greeting);
+
+var greeting = 'hi';
+console.log(greeting);
+
+var greeting = 'how are you?';
+console.log(greeting);
+console.log('====================');
+
+// let : 중복 선언 불가, 재할당 가능
+let greeting2 = 'hello';
+console.log(greeting2);
+
+// let greeting2 = 'hi';
+// console.log(greeting2);
+
+greeting2 = 'hi';
+console.log(greeting2);
+console.log('====================');
+
+// const : 중복 선언 및 재할당 불가
+const greeting3 = 'hello';
+console.log(greeting3);
+
+// const greeting3 = 'hi';
+// console.log(greeting3);
+
+// greeting3 = 'hi';
+// console.log(greeting3);
