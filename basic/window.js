4b778e0c5ed25eb6d48774212b3de7a7913e8e74
yrcho <ip323@naver.com>
2024-03-03 22:17:48 +0900

    Window Object

---
+++ basic/window.js
@@ -0,0 +1,20 @@
+let val;
+
+val = window.outerHeight;
+val = window.outerWidth;
+
+val = window.innerHeight
+val = window.innerWidth;
+
+val = window.scrollY;
+val = window.screenX;
+
+val = window.location;
+// window.location.href = "http://google.com";
+// window.history.back();
+// window.history.forward();
+// window.history.go(-1);
+
+val = window.navigator;
+
+console.log(val);
